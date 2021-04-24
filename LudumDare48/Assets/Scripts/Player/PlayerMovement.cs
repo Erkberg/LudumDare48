5aa8d804aaad05c0abfb1e7df5c60346748d4c0f
Erkberg <erisch01@web.de>
2021-04-24 12:07:20 +0200

    basic movement / rotation

--- LudumDare48/Assets/Scripts/Player/PlayerMovement.cs
+++ LudumDare48/Assets/Scripts/Player/PlayerMovement.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb2d;
+    public float moveSpeed = 1f;
+    public float maxMoveSpeed = 10f;
 
     private GameInput input;
 
@@ -16,6 +18,27 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector2 movement = new Vector2(input.GetHorizontalMovement(), input.GetVerticalMovement());
+
+        if (rb2d.velocity.sqrMagnitude < maxMoveSpeed)
+        {
+            rb2d.AddForce(movement * moveSpeed);
+        }
+
+        if (movement.sqrMagnitude > 0f)
+        {
+            Rotate(movement);
+        }
+    }
+    
+    private void Rotate(Vector2 movement)
+    {
+        float angle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 }
