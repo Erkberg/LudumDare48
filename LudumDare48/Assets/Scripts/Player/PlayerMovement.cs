4dfcba4ab62ccf52ebc7a3a6ac3112e0661e0028
Erkberg <erisch01@web.de>
2021-04-24 12:41:53 +0200

    basic dash

--- LudumDare48/Assets/Scripts/Player/PlayerMovement.cs
+++ LudumDare48/Assets/Scripts/Player/PlayerMovement.cs
@@ -8,8 +8,12 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb2d;
     public float moveSpeed = 1f;
     public float maxMoveSpeed = 10f;
+    public float dashSpeed = 100f;
+    public float dashDuration = 0.5f;
 
     private GameInput input;
+
+    private bool isDashing = false;
 
     private void Awake()
     {
@@ -25,15 +29,33 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector2 movement = new Vector2(input.GetHorizontalMovement(), input.GetVerticalMovement());
 
-        if (rb2d.velocity.sqrMagnitude < maxMoveSpeed)
+        if (rb2d.velocity.sqrMagnitude < maxMoveSpeed || isDashing)
         {
             rb2d.AddForce(movement * moveSpeed);
         }
+        
+        CheckDash(movement);
 
         if (movement.sqrMagnitude > 0f)
         {
             Rotate(movement);
         }
+    }
+
+    private void CheckDash(Vector2 movement)
+    {
+        if (!isDashing && input.GetDashButtonDown())
+        {
+            isDashing = true;
+            rb2d.AddForce(movement * dashSpeed, ForceMode2D.Impulse);
+            StartCoroutine(DashSequence());
+        }
+    }
+
+    private IEnumerator DashSequence()
+    {
+        yield return new WaitForSeconds(dashDuration);
+        isDashing = false;
     }
     
     private void Rotate(Vector2 movement)
