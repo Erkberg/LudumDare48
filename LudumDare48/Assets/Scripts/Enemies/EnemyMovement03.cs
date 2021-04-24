430c05678d6019814921fd3ea01bbbceea7bb2bf
Erkberg <erisch01@web.de>
2021-04-24 19:25:18 +0200

    add third enemy type

--- LudumDare48/Assets/Scripts/Enemies/EnemyMovement03.cs
+++ LudumDare48/Assets/Scripts/Enemies/EnemyMovement03.cs
@@ -5,9 +5,32 @@ using UnityEngine;
 
 public class EnemyMovement03 : EnemyMovement
 {
+    public float chaseDuration = 10f;
+    
+    private float chaseDurationPassed = 0f;
+    
+    public override void OnUpdate()
+    {
+        chaseDurationPassed += Time.deltaTime;
+        
+        base.OnUpdate();
+    }
+
     protected override void Move()
     {
         Vector3 moveDir = Game.inst.GetPlayerPosition() - transform.position;
-        rb2d.velocity = moveSpeed * moveDir;
+        if (ChaseEnded())
+        {
+            rb2d.velocity = -moveSpeed * moveDir.normalized;
+        }
+        else
+        {
+            rb2d.velocity = moveSpeed * moveDir.normalized;
+        }
+    }
+
+    private bool ChaseEnded()
+    {
+        return chaseDurationPassed > chaseDuration;
     }
 }
