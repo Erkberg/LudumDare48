1050055854dee7360de91128d4a33f929b723b07
Erkberg <erisch01@web.de>
2021-04-24 21:39:47 +0200

    enemy type 06

--- LudumDare48/Assets/Scripts/Enemies/EnemyMovement06.cs
+++ LudumDare48/Assets/Scripts/Enemies/EnemyMovement06.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using ErksUnityLibrary;
@@ -5,8 +6,16 @@ using UnityEngine;
 
 public class EnemyMovement06 : EnemyMovement
 {
+    private Vector2 dir;
+
+    private void Awake()
+    {
+        dir = Game.inst.GetPlayerPosition() - transform.position;
+        dir = dir.normalized;
+    }
+
     protected override void Move()
     {
-        
+        rb2d.velocity = dir * (moveSpeed * 2);
     }
 }
