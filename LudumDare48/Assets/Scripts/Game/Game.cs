b76c95fb42c0dd0fa68e6f28503c483f58b89baa
Erkberg <erisch01@web.de>
2021-04-24 08:36:04 +0200

    prepare player and game classes, add testsprites

---
+++ LudumDare48/Assets/Scripts/Game/Game.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public static Game inst;
+
+    public GameUI ui;
+    public GameProgress progress;
+
+    public PlayerController player;
+
+    private void Awake()
+    {
+        inst = this;
+    }
+}
